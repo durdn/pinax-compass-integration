fda8fd365fdad23aab35a3a05ca5a68ba1c52c22
Nicola Paolucci <durden@gmail.com>
2009-07-02 04:39:07 +0200

    Integration of Compass into a default Pinax 0.7beta2 installation
    Layout can be designed in the media/sass/*.sass files which generate the
    correct css files by invoking compass -u in the media folder.

---
+++ media/config.rb
@@ -0,0 +1,11 @@
+require 'ninesixty'
+# Require any additional compass plugins here.
+
+project_type = :stand_alone
+css_dir = "css"
+sass_dir = "sass"
+images_dir = "images"
+output_style = :compact
+# To enable relative image paths using the images_url() function:
+# http_images_path = :relative
+http_images_path = "/images"
